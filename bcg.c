e11e8747418b2a8500c84e1afa08744254baccb2
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-04-29 03:21:02 +0900

    check for extension

--- bcg.c
+++ bcg.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -10,9 +11,25 @@
 int
 main(int argc, char *argv[])
 {
+  char *ext;
+
   if (argc != 2)
   {
     fprintf(stderr, "Usage: %s <source file>\n", argv[0]);
+    return 1;
+  }
+
+  ext = strrchr(argv[1], '.');
+  if (!ext)
+  {
+    fprintf(stderr, "input path did not include a dot\n");
+    return 1;
+  }
+
+  ext++;
+  if (strcmp(ext, "bg"))
+  {
+    fprintf(stderr, "input file name did not end with `.bg'\n");
     return 1;
   }
 }
