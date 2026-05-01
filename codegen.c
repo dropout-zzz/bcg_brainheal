89af19601150c9585567580d88cb5b227ebd00a3
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-05-01 13:45:37 +0900

    add more skeletons

--- codegen.c
+++ codegen.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
 #include "compile.h"
+
+void
+gen_write_c_code(struct program *cprog, FILE *of)
+{
+}
--- compile.c
+++ compile.c
@@ -1,2 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "compile.h"
 #include "parse.h"
+
+void
+del_compiled_prog(struct program *cprog)
+{
+  free(cprog->funcs);
+}
+
+void
+del_compiler_err(struct compiler_err *cerr)
+{
+  switch (cerr->kind)
+  {
+    case DUPLICATE_FUNCTION:
+      free(cerr->duplicate_function);
+      break;
+    default:
+      printf("Fixme: may not be properly freeing unhandled compiler err %d\n", cerr->kind);
+      break;
+  }
+}
