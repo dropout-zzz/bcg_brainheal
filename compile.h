0a2bc5cc182edd89a62d1d0331316ef02191d272
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-05-01 13:45:37 +0900

    add more skeletons

--- compile.h
+++ compile.h
@@ -7,3 +7,26 @@ struct program
 {
   struct func *funcs;
 };
+
+enum
+{
+  DUPLICATE_FUNCTION,
+};
+
+struct duplicate_function
+{
+  const char *name;
+  int pos;
+};
+
+struct compiler_err
+{
+  int kind;
+  union
+  {
+    struct duplicate_function *duplicate_function;
+  };
+};
+
+void
+del_compiler_err (struct compiler_err *cerr);
